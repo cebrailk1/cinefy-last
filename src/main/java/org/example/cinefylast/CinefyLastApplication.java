b846a89d3294436a4c4cc3409c839da8fbcfd69c
Cebrail Kocakafa <cebrail.kocakafa@telefonica.com>
2025-11-03 16:12:14 +0100

    Initial commit

---
+++ src/main/java/org/example/cinefylast/CinefyLastApplication.java
@@ -0,0 +1,13 @@
+package org.example.cinefylast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinefyLastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinefyLastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/cinefylast/CinefyLastApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cinefylast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinefyLastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
